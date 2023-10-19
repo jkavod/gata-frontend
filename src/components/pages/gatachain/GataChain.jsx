f60953491b054c4dcb91505c142acebd357a8687
Neto <ukpongneto@wellahealth.com>
2023-10-19 15:55:20 +0100

    gata chain and gata business in progress

--- src/components/pages/gatachain/GataChain.jsx
+++ src/components/pages/gatachain/GataChain.jsx
@@ -3,9 +3,9 @@ import styles from '../../../style'
 
 const GataChain = () => {
   return (
-    <div className=''>
-        <div className='top-20'>
-            <h1 className='font-poppins font-semibold xs:text-[48px] text-[40px] xs:leading-[76.8px] leading-[66.8px] w-full text-gold uppercase'>Coming Soon</h1>
+    <div className='my-10'>
+        <div className='p-10'>
+            <h1 className='font-poppins font-semibold text-white uppercase'>Coming Soon</h1>
         </div>
     </div>
   )
